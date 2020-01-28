0f35c6fda6c8ff6d22dcc31d2c1cbe9ecd3464e0
SaulSandovalM <sauldevelop@gmail.com>
2020-01-28 00:17:36 -0600

    fisrt commit

---
+++ lib/screens/login/login.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Routing App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'First Page',
+              style: TextStyle(fontSize: 50),
+            ),
+            RaisedButton(
+              child: Text('Go to second'),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/second',
+                    arguments: 'Hello there from the first page!');
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
