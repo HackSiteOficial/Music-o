f09331664754079df929dc58adfdd8e865e57ea1
SaulSandovalM <sauldevelop@gmail.com>
2020-01-29 02:29:27 -0600

    Login design

--- lib/router.dart
+++ lib/router.dart
@@ -3,12 +3,11 @@ import 'package:music_finder/screens/login/login.dart';
 import 'package:music_finder/screens/home/home.dart';
 
 class RouteGenerator {
-  static Route<dynamic> generateRoute(RouteSettings settings){
+  static Route<dynamic> generateRoute(RouteSettings settings) {
     final args = settings.arguments;
-
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => FirstPage());
+        return MaterialPageRoute(builder: (_) => LoginPage());
       case '/second':
         if (args is String) {
           return MaterialPageRoute(
@@ -17,13 +16,13 @@ class RouteGenerator {
             ),
           );
         }
-        return _errorRoute();
+        /*return _errorRoute();
       default:
-        return _errorRoute();
+        return _errorRoute();*/
     }
   }
 
-  static Route<dynamic> _errorRoute() {
+  /*static Route<dynamic> _errorRoute() {
     return MaterialPageRoute(builder: (_) {
       return Scaffold(
         appBar: AppBar(
@@ -34,5 +33,5 @@ class RouteGenerator {
         ),
       );
     });
-  }
+  }*/
 }
--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,31 +1,168 @@
 import 'package:flutter/material.dart';
 
-class FirstPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  static String tag = 'login-page';
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
+    final logo = Hero(
+      tag: 'hero',
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.4,
+        child: Image.asset('assets/logo2.png'),
+        ),
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        labelText: 'Correo Electronico',
+        labelStyle: TextStyle(
+            color: Colors.white
+        ),
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18.0),
+          borderSide: BorderSide(
+              color: Colors.white, width: 1.0
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18.0),
+          borderSide: BorderSide(
+              color: Colors.white, width: 1.0
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18.0),
+          borderSide: BorderSide(
+              color: Colors.red, width: 1.0
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18.0),
+          borderSide: BorderSide(
+              color: Colors.red, width: 1.0
+          ),
+        ),
       ),
+      style: TextStyle(
+          color: Colors.white
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Contraseña',
+        suffixIcon: IconButton(
+            color: Colors.white,
+            icon: Icon(Icons.visibility_off),
+            onPressed: () {
+              debugPrint('222');
+            }
+        ),
+        labelStyle: TextStyle(
+            color: Colors.white
+        ),
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0)
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18.0),
+          borderSide: BorderSide(
+              color: Colors.white, width: 1.0
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18.0),
+          borderSide: BorderSide(
+              color: Colors.white, width: 1.0
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18.0),
+          borderSide: BorderSide(
+              color: Colors.red, width: 1.0
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18.0),
+          borderSide: BorderSide(
+              color: Colors.red, width: 1.0
+          ),
+        ),
+      ),
+      style: TextStyle(
+          color: Colors.white
+      ),
+    );
+
+    final forgotLabel = FlatButton(
+      padding: EdgeInsets.all(0.0),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Text(
+          '¿Olvidaste tu contraseña?',
+          style: TextStyle(color: Colors.white70),
+        ),
+      ),
+      onPressed: () {},
+    );
+
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 50.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+          //side: BorderSide(color: Colors.grey)
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed('/second',
+              arguments: 'Home');
+        },
+        padding: EdgeInsets.all(12),
+        color: Colors.grey,
+        child: Text('Iniciar Sesión', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 30.0, right: 30.0),
           children: <Widget>[
-            Text(
-              'First Page',
-              style: TextStyle(fontSize: 50),
-            ),
-            RaisedButton(
-              child: Text('Go to home'),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/second',
-                    arguments: 'Hello there from the first page!');
-              },
-            )
+            logo,
+            SizedBox(height: 50.0),
+            email,
+            SizedBox(height: 25.0),
+            password,
+            SizedBox(height: 5.0),
+            forgotLabel,
+            SizedBox(height: 30.0),
+            loginButton
           ],
         ),
       ),
     );
   }
 }
+/*
+
+
+onPressed: () {
+Navigator.of(context).pushNamed('/second',
+arguments: 'Hello there from the first page!');
+},*/
