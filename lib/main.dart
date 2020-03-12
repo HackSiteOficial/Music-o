d6212aaf75cdaa5d823e79af42f4b03a7acc156b
SaulSandovalM <sauldevelop@gmail.com>
2020-03-12 15:14:55 -0600

    Perfect login (i think) and deleted files

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:music_finder/router.dart';
-
-void main() => runApp(MyApp());
+import 'package:music_finder/util/state_widget.dart';
+import 'package:music_finder/screens/login/redirector.dart';
+import 'package:music_finder/screens/login/sign_in.dart';
+import 'package:music_finder/screens/login/sign_up.dart';
+import 'package:music_finder/screens/login/forgot_password.dart';
 
 class MyApp extends StatelessWidget {
+  MyApp() {
+    //Navigation.initPaths();
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Music'o",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/profile',
-      onGenerateRoute: RouteGenerator.generateRoute,
+      routes: {
+        '/': (context) => Redirector(),
+        '/signin': (context) => SignIn(),
+        '/signup': (context) => SignUp(),
+        '/forgot-password': (context) => ForgotPassword(),
+      },
     );
   }
 }
 
+void main() {
+  StateWidget stateWidget = new StateWidget(
+    child: new MyApp(),
+  );
+  runApp(stateWidget);
+}
